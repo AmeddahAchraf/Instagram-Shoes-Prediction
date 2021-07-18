7bc067bf69b13424b730825f44797a7f618f1357
AmeddahAchraf <fm_ameddah@esi.dz>
2021-07-18 10:53:17 +0200

    [Fix] files structure

--- Instagram-MVVM/MainTabBarController.swift
+++ Instagram-MVVM/MainTabBarController.swift
@@ -11,7 +11,6 @@ class MainTabBarController: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("hello")
         setupViewControllers()
     }
     
